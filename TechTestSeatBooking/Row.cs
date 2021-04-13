3ff64b9d5007d4d142f8e92460108a144ff46f1a
Ewan Graham <EwanG@acuigen.com>
2021-04-13 10:32:04 +0100

    Add project files.

---
+++ TechTestSeatBooking/Row.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace TechTestSeatBooking
+{
+    public class Row
+    {
+
+        public int RowNumber { get; set; }
+        public List<Seat> Seats { get; set; }
+
+        public int CountOfEmptySeats()
+        {
+            //int emptySeats = 0;
+
+            //foreach (Seat seat in Seats)
+            //{
+            //    if (seat.IsAllocated == false)
+            //    {
+            //        emptySeats++;
+            //    }
+            //}
+
+            //return emptySeats;
+
+            return Seats.Where(seat => !seat.IsAllocated).Count();
+        }
+
+        public Row(int rowNumber)
+        {
+            RowNumber = rowNumber;
+            Seats = new List<Seat>();
+
+            for (int i = 1; i <= 6; i++)
+            {
+                Seat seat = new Seat();
+                seat.SeatNumber = i;
+                seat.IsAllocated = false;
+
+                Seats.Add(seat);
+            }
+            
+            
+        }
+            
+
+    }
+}
---
+++ TechTestSeatBooking/Seat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechTestSeatBooking
+{
+    public class Seat
+    {
+        public int SeatNumber { get; set; }
+        public bool IsAllocated { get; set; }
+    }
+}
